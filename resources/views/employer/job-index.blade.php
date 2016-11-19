b8977c36287253011d8a4a4aec0596399b376000
Satrya Rangga Buana <satrya.rangga@olx.co.id>
2016-11-20 04:33:59 +0700

    email

--- resources/views/employer/job-index.blade.php
+++ resources/views/employer/job-index.blade.php
@@ -82,7 +82,7 @@
                                             </div>
 
                                             <strong class="price"><i class="fa fa-money"></i>{{\App\Helpers\GlobalHelper::moneyFormat($row['salary_min'])}} - {{\App\Helpers\GlobalHelper::moneyFormat($row['salary_max'])}}</strong>
-                                            @if($authRole != 'employer') <a href="{{route('job-apply', ['jobId' => $row['id']])}}" onclick="return confirm('Anda akan melamar pekerjaan {{$row['title']}} di {{$row['employerName']}}. Lanjutkan Proses ?')" class="btn-1 btn-color-1 ripple">Lamar Pekerjaan</a> @endif
+                                            @if($authRole != 'employer') <a href="{{($authRole == 'worker') ? route('job-apply', ['jobId' => $row['id']]) : route('login')}}" onclick="@if($authRole == 'worker') return confirm('Anda akan melamar pekerjaan {{$row['title']}} di {{$row['employerName']}}. Lanjutkan Proses ?') @else alert('Silahkan login sebagai pekerja untuk melamar') @endif " class="btn-1 btn-color-1 ripple">Lamar Pekerjaan</a> @endif
                                         </div>
 
                                     </li>
