873736adf488170f1ba05b8b8c671937a08e9e88
Satrya Rangga Buana <satrya.rangga@olx.co.id>
2016-12-08 02:32:23 +0700

    new template

--- config/view.php
+++ config/view.php
@@ -15,7 +15,7 @@ return [
 
 
     'paths' => [
-        realpath(base_path('resources/views/'.config('app.template'))),
+        realpath(base_path('resources/views/'.env('TEMPLATE_NAME'))),
     ],
 
     /*
