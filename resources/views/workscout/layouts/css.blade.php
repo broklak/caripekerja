ba3e94d3d293422381340e55c30773f6d59bff25
Satrya Rangga Buana <satrya.rangga@olx.co.id>
2016-12-09 21:29:20 +0700

    home edit

--- resources/views/workscout/layouts/css.blade.php
+++ resources/views/workscout/layouts/css.blade.php
@@ -2,6 +2,6 @@
 <!--CUSTOM CSS-->
 
 <link rel="stylesheet" href="{{ asset("css") }}/styles.css?version=3.1">
-<link rel="stylesheet" href="{{ asset("css") }}/colors/blue.css?version=2.0" id="colors">
+<link rel="stylesheet" href="{{ asset("css") }}/colors/blue.css?version=2.5" id="colors">
 
 @includeIf($css_name)
