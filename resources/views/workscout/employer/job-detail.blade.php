52ae726df91e7a8c15c477d34431851c0ba1ecbc
Satrya Rangga Buana <satrya.rangga@olx.co.id>
2016-12-18 13:42:26 +0700

    revamped profil

--- resources/views/workscout/employer/job-detail.blade.php
+++ resources/views/workscout/employer/job-detail.blade.php
@@ -9,27 +9,29 @@
             <img src="{{asset("images")}}/user/bg-profil-kerja.jpg" alt="">
             <div class="mj_mainheading_overlay"></div>
         </div>
-        <div class="container">
+        <div class="container-mesh">
             <div class="mj_pagetitle_inner">
                 <div class="container-mesh">
                     <div class="row">
                         <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
                             <div class="mj_mainheading">
                                 <div class="row">
-                                    <div class="col-lg-2 col-md-2 col-sm-2 col-xs-12">
+                                    <div class="col-lg-2 col-md-2 col-sm-2 col-lg-offset-1 col-xs-12">
                                         <div class="mj_joblogo employer">
                                             <img src="{{\App\Helpers\GlobalHelper::setEmployerImage($detail['employerPhoto'])}}" class="img-responsive" alt="">
                                         </div>
                                     </div>
-                                    <div class="col-lg-10 col-md-10 col-sm-10 col-xs-12">
-                                        <div class="mj_pageheading" style="margin-left: 20px">
-                                            <h1>{{$detail['title']}}</h1>
-                                            <ul>
-                                                <li><i class="fa fa-clock-o"></i> {{($detail['type'] == 1) ? ' Full Time' : 'Part Time'}}</li>
-                                                <li><i class="fa fa-map-marker"></i> {{$detail['provinceName']}}</li>
-                                                <li><i class="fa fa-calendar"></i> Diposting {{\App\Helpers\GlobalHelper::getHowLongTime($detail['created_at'])}}</li>
-                                                <li><i class="fa fa-money"></i> {{\App\Helpers\GlobalHelper::moneyFormat($detail['salary_min'])}} - {{\App\Helpers\GlobalHelper::moneyFormat($detail['salary_max'])}}</li>
-                                            </ul>
+                                    <div class="col-lg-9 col-md-9 col-sm-9 col-xs-12">
+                                        <div class="mj_pageheading">
+                                            <div class="job-profil-heading">
+                                                <h1>{{$detail['title']}}</h1>
+                                                <ul>
+                                                    <li><i class="fa fa-clock-o"></i> {{($detail['type'] == 1) ? ' Full Time' : 'Part Time'}}</li>
+                                                    <li><i class="fa fa-map-marker"></i> {{$detail['provinceName']}}</li>
+                                                    <li><i class="fa fa-calendar"></i> {{\App\Helpers\GlobalHelper::getHowLongTime($detail['created_at'])}}</li>
+                                                    <li><i class="fa fa-money"></i> {{\App\Helpers\GlobalHelper::moneyFormat($detail['salary_min'])}} - {{\App\Helpers\GlobalHelper::moneyFormat($detail['salary_max'])}}</li>
+                                                </ul>
+                                            </div>
                                         </div>
                                     </div>
                                 </div>
@@ -48,11 +50,13 @@
             <div class="row">
                 <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
                     <div class="mj_social_media_section mj_candidatepage_media mj_toppadder40 mj_bottompadder40">
-                        @if($authRole != 'employer')
-                            <a class="contact-candidate" href="{{($authRole == 'worker') ? route('job-apply', ['jobId' => $detail['id']]) : route('login')}}"
-                               onclick="@if($authRole == 'worker') return confirm('Anda akan melamar pekerjaan {{$detail['title']}} di {{$detail['employerName']}}. Lanjutkan Proses ?') @else alert('Silahkan login sebagai pekerja untuk melamar') @endif "
-                               data-text="Contact Candidate">LAMAR PEKERJAAN</a>
-                        @endif
+                        <div class="button-lamar-job">
+                            @if($authRole != 'employer')
+                                <a class="contact-candidate" href="{{($authRole == 'worker') ? route('job-apply', ['jobId' => $detail['id']]) : route('login')}}"
+                                   onclick="@if($authRole == 'worker') return confirm('Anda akan melamar pekerjaan {{$detail['title']}} di {{$detail['employerName']}}. Lanjutkan Proses ?') @else alert('Silahkan login sebagai pekerja untuk melamar') @endif "
+                                   data-text="Contact Candidate">LAMAR PEKERJAAN</a>
+                            @endif
+                        </div>
                     </div>
                 </div>
             </div>
@@ -60,7 +64,7 @@
             <div class="row">
                 <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
 
-                    <div class="col-lg-6 col-md-6 col-sm-12 col-xs-12">
+                    <div class="col-lg-6 col-md-6 col-sm-6 col-xs-12">
                         <div class="mj_postdiv mj_jobdetail mj_toppadder20 mj_bottompadder50">
                             <div class="">
                                 <div class="padding-right">
@@ -71,7 +75,7 @@
                         </div>
                     </div>
 
-                    <div class="col-lg-6 col-md-6 col-sm-12 col-xs-12">
+                    <div class="col-lg-6 col-md-6 col-sm-6 col-xs-12">
                         <div class="mj_postdiv mj_jobdetail mj_toppadder20 mj_bottompadder50">
                             <div class="">
                                 <div class="padding-right">
